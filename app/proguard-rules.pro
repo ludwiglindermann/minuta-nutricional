651eaa2d2236ac5eb12452e9503fb266d76cc0d3
Ludwig Lindermann <ludwig.lindermann.c@gmail.com>
2026-08-18 00:01:02 +0000

    chore: estructura base del proyecto Android (Gradle, manifest, tema y recursos)

---
+++ app/proguard-rules.pro
@@ -0,0 +1,3 @@
+# Add project specific ProGuard rules here.
+# Reglas por defecto generadas por Android Studio.
+# https://developer.android.com/studio/build/shrink-code
---
+++ build.gradle.kts
@@ -0,0 +1,5 @@
+// Top-level build file donde se agregan opciones de configuración comunes a todos los subproyectos/módulos.
+plugins {
+    id("com.android.application") version "8.5.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
+}
